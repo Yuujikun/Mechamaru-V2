ab02cf546359158a48ce416b59cd4e7d354b1a15
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-05-06 23:23:58 +0100

    Update GPT4.js

--- plugins/GPT4.js
+++ plugins/GPT4.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 }
 handler.help = ['gemini <text>']
 handler.tags = ['tools']
-handler.command = /^(gpt4)$/i
+handler.command = /^(mei)$/i
 
 export default handler
 
