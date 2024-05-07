c30d2d89510a32494ff949814a56b6e88e48bad4
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-05-07 19:28:48 +0100

    Update main-menu4.js

--- plugins/main-menu4.js
+++ plugins/main-menu4.js
@@ -698,7 +698,7 @@ import {
   
   handler.help = ["play"];
   handler.tags = ["downloader"];
-  handler.command = /^(menu4)$/i;
+  handler.command = /^(demomenu)$/i;
   handler.limit = true;
   export default handler;
   
