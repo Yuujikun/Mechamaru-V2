5d76d26eb8796bd4f09fa8042d4418c39c9024fb
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-05-07 13:40:56 +0100

    Update fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -5,7 +5,7 @@ function handler(m, { groupMetadata }) {
     let b
     do b = ps.getRandom()
     while (b === a)
-    m.reply(`${toM(a)} ❤️ ${toM(b)}\nCongratulations 💖🍻`, null, {
+    m.reply(`${toM(a)} ❤️ ${toM(b)}\nCongratulations,I found ur perfect match💖🍻`, null, {
         mentions: [a, b]
     })
 }
