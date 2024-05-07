42c2b4f0b5d7fe86ff7a36de7811813310047150
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-05-07 18:33:11 +0100

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -5,7 +5,7 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./Assets/mp3/${pickRandom(["Xlicon", "Xlicon1", "Xlicon2", "Xlicon3"])}.mp3`
 
-m.reply( `Hello ${taguser} Am Mei Mei v1 creatd by Blac Yuji, Need help?  type .help `)
+m.reply( `Hello ${taguser} im Mei Mei v1,a hot ahh Whatsapp artificial assistant, Need help?  type .help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
