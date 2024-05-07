27068f6f587046a5d81adc87f9b6850da82efb84
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-05-07 14:02:21 +0100

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -5,9 +5,9 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     // Sound
     let name = m.pushName || conn.getName(m.sender)
     var vn = "./Assets/ALIVE.mp3"
-    let url = "https://github.com/abrahamdw882"
-    let murl = "https://YouTube.com"
-    let img = "https://telegra.ph/file/b1b157e944010efebf1d7.jpg"
+    let url = "https://whatsapp.com/channel/0029Va8VuWo4o7qRFa9pEF47"
+    let murl = "https://whatsapp.com/channel/0029Va8VuWo4o7qRFa9pEF47"
+    let img = "https://i.imgur.com/uBu2Xj1.jpeg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
