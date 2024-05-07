43fc2e37f09b99c198903750b500aa8d1b5e6b49
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-05-07 18:36:01 +0100

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -9,7 +9,7 @@ m.reply( `Hello ${taguser} im Mei Mei v1,a hot ahh Whatsapp artificial assistant
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(Xlicon|Team Xlicon)$/i
+handler.customPrefix = /^(Meimei)$/i
 handler.command = new RegExp
 
 export default handler
