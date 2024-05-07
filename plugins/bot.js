d862c2a82f2dc3dbe931a34a7a8398e4164b6d48
Yuujikun <151445583+Yuujikun@users.noreply.github.com>
2024-05-07 18:31:57 +0100

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/${pickRandom(["Xlicon", "Xlicon1", "Xlicon2", "Xlicon3", "Xlicon4"])}.mp3`
+let av = `./Assets/mp3/${pickRandom(["Xlicon", "Xlicon1", "Xlicon2", "Xlicon3"])}.mp3`
 
 m.reply( `Hello ${taguser} Am Mei Mei v1 creatd by Blac Yuji, Need help?  type .help `)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
